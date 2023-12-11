7d29cb7c7c0a951596ec310292b307f904ca38e2
21B1NUM1961 <143790646+21B1NUM1961@users.noreply.github.com>
2023-12-11 14:15:23 +0800

    but2 duussaa

---
+++ but2.js
@@ -0,0 +1,77 @@
+// product_detail jsonbin file
+const apiUrl = "https://api.jsonbin.io/v3/b/657536ff0574da7622d2d050"
+
+// ene functionoor page ee shinechlene parameteree huleej avaad info haruulas section doo bicsn htmlee renderlesen 
+document.addEventListener("DOMContentLoaded", function () {
+    const urlParams = new URLSearchParams(window.location.search);
+    const animeId = urlParams.get("id");
+    fetch(apiUrl).then(response => response.json()).then(data => {
+        let animes = data.record;
+        const Product = getAnimeById(animeId, animes);
+        const animeInfoSection = document.getElementById("renderAnimeInfo");
+        animeInfoSection.innerHTML = `
+        <section class="item_page">
+            <article id="item">
+                <figure id="product-image"><img src="${Product.image}" alt="Bread"></figure>
+                <div id="price-add">
+                    <p> ${Product.price} </p>
+                    <button>Сагслах</button>
+                </div>
+            </article>
+            
+           <section class="c0ontainer">
+            <article id="product-information" >
+                <h2>${Product.Name}</h2>
+                <h3>Шим тэжээл</h3>
+                <article class="div">
+                    <p>Калори</p>
+                    <p class="items_1">${Product.Calories}</p>
+                    <p>Нийт өөх тос</p>
+                    <p class="items_1">${Product.Total_fat}</p>
+                    <p>Ханасан өөх тос</p>
+                    <p class="items_1">${Product.Satured_fat}</p>
+                    <p>Транс тос</p>
+                    <p class="items_1">${Product.Trans_fat}</p>
+                    <p>Нийт нүүрс ус</p>
+                    <p class="items_1">${Product.Total_Carbohybrate}</p>
+                    <p>Нийт сахар</p>
+                    <p class="items_1">${Product.Total_Sugar}</p>
+                    <p>Уураг</p>
+                    <p class="items_1">${Product.Protein}</p>
+                  
+                    <hr class="div_hr">
+                    <hr class="div_hr">
+                </article>
+                
+            </article>
+            <article id="allergens">
+                <h3>Харшил өгч болох зүйлс</h3>
+                <hr>
+                    <div id="allergens-icons">
+                        <img alt="" loading="lazy" class="allergy-icon" src="https://assets-global.website-files.com/649249d29a20bd6bc3deac48/649249d29a20bd6bc3deac5b_egg-allergy.svg">
+                        <img alt="" loading="lazy" class="allergy-icon" src="https://assets-global.website-files.com/649249d29a20bd6bc3deac48/649249d29a20bd6bc3deac97_dairy-allergy.svg">
+                        <img alt="" loading="lazy" class="allergy-icon" src="https://assets-global.website-files.com/649249d29a20bd6bc3deac48/649249d29a20bd6bc3deac59_soy-allergy.svg">
+                        <img alt="" loading="lazy" class="allergy-icon" src="https://assets-global.website-files.com/649249d29a20bd6bc3deac48/649249d29a20bd6bc3deac6f_wheat-allergy.svg">
+                        <img alt="" loading="lazy" class="allergy-icon" src="https://assets-global.website-files.com/649249d29a20bd6bc3deac48/649249d29a20bd6bc3deac5a_tree-nut-allergy.svg">
+                    </div>
+                    <p><span>Хэмжээ :  ${Product.Size}</span></p>
+                    <p><span>Хадгалах хугацаа : ${Product.Storage_time}</span></p>
+                <p id="entrance"><span>Найрлага:</span> ${Product.Composition}</p>
+            </article>
+            </section>
+        </section>
+        `;
+    })
+});
+
+// enuuugeer jsonoo oosoo anime name r ni anime infogo avsan 
+function getAnimeById(animeId, list) {
+    for (let i = 0; i < list.length; i++) {
+        for (let j = 0; j < list[i].prods.length; j++) {
+            if (list[i].prods[j].id == animeId) {
+                return list[i].prods[j];
+            }
+        }
+    }
+}
+
--- product.js
+++ product.js
@@ -11,7 +11,7 @@ const par = params.get("category");
 
 async function fetchJSON() {
     const response = await fetch(
-        "https://api.jsonbin.io/v3/b/6567cf0a54105e766fd76f5f"
+        "https://api.jsonbin.io/v3/b/657536ff0574da7622d2d050"
     );
     const datasa = await response.json();
     const data = datasa.record;
@@ -26,22 +26,25 @@ async function fetchJSON() {
 
 class Product {
     constructor(product) {
-        this.image = product.image
+        this.id = product.id;
+        this.image = product.image;
         this.name = product.Name;
         this.desc = product.Description;
-        this.price = product.price;
+        this.price = product.price
     }
+
+
 
     RenderAll() {
         return `
-        <article>
+        <article onclick="moveToProductDetailPage('${this.id}')" style="cursor: pointer;">
             <figure><img src="${this.image}" alt="products"></figure>
             <h3>${this.name}</h3>
             <hr>
             <p>${this.desc}</p>
             <div>
                 <p>${this.price}</p>
-                <button>Сагслах</button>
+                <button >Сагслах</button>
             </div>
         </article>
         `;
@@ -84,6 +87,7 @@ class App {
                 myhtml += `<section class="cake">`
                 for (const buteegdehuun of cat.prods) {
                     const product = new Product(buteegdehuun);
+                    console.log(buteegdehuun);
                     myhtml += product.RenderAll();
                 }
                 myhtml += "</section>"
@@ -110,7 +114,16 @@ class App {
     }
 }
 
-
-
 const app = new App();
 app.init();
+
+
+// enuuger but2-luu dataga url aar damjulad avn deer render dree onclick der ni zaagd ugcin
+moveToProductDetailPage = function(id) {
+    console.log("hello");
+    let currentURL = window.location.href;
+    const urlParams2 = new  URL(window.location.href);
+    let newUrl = urlParams2.origin;
+    newUrl += `/but2.html?id=${id}`
+    window.location.href = newUrl;
+  }
