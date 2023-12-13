a2b31bb19af41d1e5b312e318d66e8b42815c847
21B1NUM1961 <143790646+21B1NUM1961@users.noreply.github.com>
2023-12-13 09:29:59 +0800

    Update product.js

--- product.js
+++ product.js
@@ -124,6 +124,6 @@ moveToProductDetailPage = function(id) {
     let currentURL = window.location.href;
     const urlParams2 = new  URL(window.location.href);
     let newUrl = urlParams2.origin;
-    newUrl += `/tlj/but2.html?id=${id}`
+    newUrl += `/tlj/but2.html?id=${id}`;
     window.location.href = newUrl;
   }
